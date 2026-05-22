672b2ba9bef1a4163a059d1a8c4d1d9c7ae37dc5
junaidasims <junaidasim2404@gmail.com>
2026-05-22 22:48:55 +0530

    fix: revert vite config to default

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,13 +8,4 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name]-[hash]-v2.js`,
-        chunkFileNames: `assets/[name]-[hash]-v2.js`,
-        assetFileNames: `assets/[name]-[hash]-v2.[ext]`
-      }
-    }
-  }
 })
